a9475104d66e28bf7e0d111790b59a8c0e2ba142
Sergey <Serduyksb@gimai.com>
2022-02-16 10:02:25 +0300

    Initial commit test Spring Boot API

---
+++ sber_test_user_api/src/main/java/ru/serdyuk/SberTestUserApiApplication.java
@@ -0,0 +1,13 @@
+package ru.serdyuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTestUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberTestUserApiApplication.class, args);
+	}
+
+}
