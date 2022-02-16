b17b84c1fdfaa2bd10c9b9946ff30a879082b99f
Sergey <Serduyksb@gimai.com>
2022-02-16 11:01:42 +0300

    Create converter from Users -> Dto

--- sber_test_user_api/src/main/java/ru/serdyuk/service/DefaultUsersService.java
+++ sber_test_user_api/src/main/java/ru/serdyuk/service/DefaultUsersService.java
@@ -1,13 +1,29 @@
 package ru.serdyuk.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.serdyuk.dto.UsersDto;
+import ru.serdyuk.entity.Users;
+import ru.serdyuk.repository.UserRepository;
 
 @Service
+@AllArgsConstructor
 public class DefaultUsersService implements UserService{
+
+    private final UserRepository userRepository;
+
+
+
     @Override
     public UsersDto saveUser(UsersDto usersDto) {
-        return null;
+        Users users = new Users();
+        users.setId(usersDto.getId());
+        users.setLogin(usersDto.getLogin());
+        users.setName(usersDto.getName());
+        users.setEmail(usersDto.getEmail());
+        users.setBirthday(users.getBirthday());
+
+        return userRepository.save(users);
     }
 
     @Override
---
+++ sber_test_user_api/src/main/java/ru/serdyuk/service/UsersConverter.java
@@ -0,0 +1,28 @@
+package ru.serdyuk.service;
+
+import org.springframework.stereotype.Component;
+import ru.serdyuk.dto.UsersDto;
+import ru.serdyuk.entity.Users;
+
+@Component
+public class UsersConverter {
+
+    public Users fromDtoToEntity(UsersDto usersDto) {
+        Users users = new Users();
+        users.setId(usersDto.getId());
+        users.setEmail(usersDto.getEmail());
+        users.setName(usersDto.getName());
+        users.setLogin(usersDto.getLogin());
+        return users;
+    }
+
+    public UsersDto fromEntityToDto(Users users) {
+        return UsersDto.builder()
+                .id(users.getId())
+                .email(users.getEmail())
+                .login(users.getLogin())
+                .name(users.getName())
+                .build();
+    }
+
+}
